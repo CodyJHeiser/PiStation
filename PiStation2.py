d84f410e9d3a5e95f7d931d43a16b953d5cd17a1
Tyler Dinsmoor <pappad@airmail.cc>
2015-04-09 21:20:27 +0900

    Added argument parsing, making file selection easier; Removed unneeded logic.

--- PiStation2.py
+++ PiStation2.py
@@ -1,7 +1,11 @@
-#!/bin/python
+#!/usr/bin/python
 
 import os, argparse
-current_dir = os.system("pwd") #Shows current directory
+
+parser = argparse.ArgumentParser(prog='python PiStation.py', description='Broadcasts WAV/MP3 file over FM using RPI GPIO #4 pin.')
+parser.add_argument("song_file")
+parser.add_argument("-f", "--frequency", help="Set TX frequency. Acceptable range 87.1-108.2", type=float)
+arg = parser.parse_args()
 
 HEADER = '\033[95m'
 OKBLUE = '\033[94m'
@@ -12,67 +16,33 @@ ENDC = '\033[0m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
 
-
-def findfiles(filename):
-    base = "/home/pi"
-    target = filename
-    global SongPath
-    for root, dirs, files in os.walk(base):
-        if target in files:
-            print "File Found!"
-            SongPath = os.path.join(root, target)
-            return
-        else:
-            SongPath = "Sorry the file, " + WARNING + SongName + ENDC + " doesn\'t seem to exist" #holding a value (required)
-            #You can un-commnet 'SongPath' here (below), but it repeats a coulple of times when searching
-            #print SongPath
-            SongPath = "0"
-
-def find_type(Song_Path):
-    global Song_type
-    if ".mp3" in Song_Path.lower():
-        Song_type = "001"
-    elif ".wav" in Song_Path.lower():
-        Song_type = "002"
-    else:
-        Song_type = "003"
-        print "Sorry that file extension isn\'t supported!"
-        exit()
-
-
 def Station_Selector():
-    freq = None
-    while 87.1 >= freq >= 108.2:
-        print "Please enter your Frequency, anywhere between 87.1 and 108.2"
-        freq = float(raw_input("> "))
+    if arg.frequency 87.1 <= freq <= 108.2: freq = arg.frequency
+    else:
+        print "Frequency argument out of range."
+        freq = 0
+        while 87.1 <= freq <= 108.2:
+            print "Please enter your Frequency, anywhere between 87.1 and 108.2"
+            freq = float(raw_input("> "))
     try:
-        loading()
-        if Song_type == "001": #MP3 File
+        if ".mp3" in args.song_file.lower():
             os.system("ffmpeg -i " + SongPath + " -f s16le -ar 22.05k -ac 1 - | sudo ./pifm - " + str(freq))
-        elif Song_type == "002": #WAV FILE
+        elif ".wav" in args.song_file.lower():
             os.system("sudo ./pifm " + SongPath + " " + str(freq) + " 22050 stereo")
+        else:
+            print "Sorry that file extension isn't supported or the file does not exist!"
+            print "File name provided: %s" %(WARNING + SongName + ENDC)
+            exit()
     except Exception:
         print "There was an exception. Halting."
         exit()
 
-parser = argparse.ArgumentParser(prog='python PiStation.py', description='Broadcasts WAV/MP3 file over FM using RPI GPIO #4 pin.')
 
 
 def main():
-    os.system('clear')
+    #os.system('clear')
     print ("Welcome to ImPiFM!  \nVersion 1.0 \nGPLv3 License")
-    os.system('clear')
-    print ("Enter the file you would like to play here! \nWatch Capitalization!")
-    global SongName
-    SongName = raw_input("\n")
-
-    findfiles(SongName)
-    if SongPath == "0":
-        os.system('clear')
-        print "Sorry, couldn\'t find your file!  Please try again!"
-        exit()
-    os.system('clear')
-    find_type(SongPath)
+    #os.system('clear')
     Station_Selector()
     
 if __name__ == '__main__':
