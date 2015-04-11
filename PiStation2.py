f8cd7dd0f306b62bea2e302dae38afcb44141322
Tyler Dinsmoor <pappad@airmail.cc>
2015-04-11 11:39:16 +0900

    Optimized more

--- PiStation2.py
+++ PiStation2.py
@@ -7,43 +7,24 @@ parser.add_argument("song_file")
 parser.add_argument("-f", "--frequency", help="Set TX frequency. Acceptable range 87.1-108.2", type=float)
 arg = parser.parse_args()
 
-HEADER = '\033[95m'
-OKBLUE = '\033[94m'
-OKGREEN = '\033[92m'
-WARNING = '\033[93m'
-FAIL = '\033[91m'
-ENDC = '\033[0m'
-BOLD = '\033[1m'
-UNDERLINE = '\033[4m'
-
-def Station_Selector():
-    if arg.frequency 87.1 <= freq <= 108.2: freq = arg.frequency
-    else:
-        print "Frequency argument out of range."
-        freq = 0
-        while 87.1 <= freq <= 108.2:
-            print "Please enter your Frequency, anywhere between 87.1 and 108.2"
-            freq = float(raw_input("> "))
-    try:
-        if ".mp3" in args.song_file.lower():
-            os.system("ffmpeg -i " + SongPath + " -f s16le -ar 22.05k -ac 1 - | sudo ./pifm - " + str(freq))
-        elif ".wav" in args.song_file.lower():
-            os.system("sudo ./pifm " + SongPath + " " + str(freq) + " 22050 stereo")
-        else:
-            print "Sorry that file extension isn't supported or the file does not exist!"
-            print "File name provided: %s" %(WARNING + SongName + ENDC)
-            exit()
-    except Exception:
-        print "There was an exception. Halting."
-        exit()
-
-
-
 def main():
-    #os.system('clear')
-    print ("Welcome to ImPiFM!  \nVersion 1.0 \nGPLv3 License")
-    #os.system('clear')
-    Station_Selector()
+    os.system('clear')
+    print ("Welcome to ImPiFM!  \nVersion 1.0 \nGPLv3 License\n")
+    if 87.1 >= arg.frequency >= 108.2:
+        print "Frequency argument out of range.";exit()
+    try:
+        if ".mp3" in arg.song_file.lower():
+            os.system("ffmpeg -i %s -f s16le -ar 22.05k -ac 1 - | sudo ./pifm - %s") %(arg.song_file, str(arg.frequency))
+        elif ".wav" in arg.song_file.lower():
+            os.system("sudo ./pifm %s %s 22050 stereo") %(arg.song_file, str(arg.frequency))
+        else:
+            print "That file extension is not supported."
+            print "File name provided: %s" %arg.song_file
+            raise IOError
+    except Exception:
+        print "Something went wrong. Halting."; exit()
+    except IOError:
+        print "There was an error regarding file selection. Halting."; exit()
     
 if __name__ == '__main__':
     main()
